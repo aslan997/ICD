fcabe78921ff6c6a02b8925da531bd41b2a7c804
Hasnat Amin <hasnatamin065@gmail.com>
2024-07-23 12:16:28 +0500

    dropdown is added

--- src/pages/PerformanceManagementForm/components/KPITable.jsx
+++ src/pages/PerformanceManagementForm/components/KPITable.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import KPIInformationTable from "./KPIInformationTable";
 import MYRTable from "./MYRTable";
 import YERTable from "./YERTable";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 const KPITable = () => {
+  const [isTbodyVisible, setIsTbodyVisible] = useState(true); // State to manage visibility of tbody
+
+  const toggleTbodyVisibility = () => {
+    setIsTbodyVisible(!isTbodyVisible);
+  };
   return (
     <div className="rounded-md mb-4">
       <table className="min-w-full">
         <thead>
-          <tr className="bg-cayon">
+          <tr className="bg-cayon relative">
             <th className="w-35 px-4 py-2 text-center text-14 font-head">
               KPI Information
             </th>
@@ -18,14 +24,34 @@ const KPITable = () => {
             <th className="w-41 px-4 py-2 text-center text-14 font-head bg-pineGlade">
               YER
             </th>
+            <th className="absolute right-4 top-2 cursor-pointer">
+              {isTbodyVisible ? (
+                <Icon
+                  icon="iconamoon:arrow-down-2-light"
+                  onClick={toggleTbodyVisibility}
+                />
+              ) : (
+                <Icon
+                  icon="iconamoon:arrow-up-2-light"
+                  onClick={toggleTbodyVisibility}
+                />
+              )}
+            </th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <KPIInformationTable />
-            <MYRTable />
-            <YERTable />
-          </tr>
+
+        <tbody
+          className={`transition-all duration-500 ${
+            isTbodyVisible ? "opacity-100 max-h-full" : "opacity-0 max-h-0"
+          }`}
+        >
+          {isTbodyVisible && (
+            <tr>
+              <KPIInformationTable />
+              <MYRTable />
+              <YERTable />
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
--- src/pages/PerformanceManagementForm/components/ObjectTable.jsx
+++ src/pages/PerformanceManagementForm/components/ObjectTable.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import KPITable from "./KPITable";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 const ObjectTable = () => {
+  const [isTbodyVisible, setIsTbodyVisible] = useState(true); // State to manage visibility of tbody
+
+  const toggleTbodyVisibility = () => {
+    setIsTbodyVisible(!isTbodyVisible);
+  };
   return (
     <div className="overflow-x-auto mb-4">
       <table className="min-w-full ">
         <thead>
-          <tr className="bg-cayon border-b">
+          <tr className="bg-cayon border-b relative">
             <th className="px-4 py-2 text-left text-14 font-head">
               Object Code
             </th>
@@ -16,9 +22,26 @@ const ObjectTable = () => {
             <th className="px-4 py-2 text-left text-14 font-head">
               Object Text
             </th>
+            <th className="absolute right-4 top-2 cursor-pointer">
+              {isTbodyVisible ? (
+                <Icon
+                  icon="iconamoon:arrow-down-2-light"
+                  onClick={toggleTbodyVisibility}
+                />
+              ) : (
+                <Icon
+                  icon="iconamoon:arrow-up-2-light"
+                  onClick={toggleTbodyVisibility}
+                />
+              )}
+            </th>
           </tr>
         </thead>
-        <tbody>
+        <tbody
+          className={`transition-all duration-500 ${
+            isTbodyVisible ? "opacity-100 max-h-full" : "opacity-1 max-h-full"
+          }`}
+        >
           <tr className="bg-alabaster">
             <td className="px-4 py-2 text-sm">43623</td>
             <td className="px-4 py-2 text-sm">98763</td>
@@ -27,12 +50,16 @@ const ObjectTable = () => {
               Ut enim ad minim veniam, quis nostrud
             </td>
           </tr>
-          <tr className="bg-alabaster border-b">
-            <td colSpan="3" className=" p-4">
-              <KPITable />
-              <KPITable />
-            </td>
-          </tr>
+          {isTbodyVisible && (
+            <>
+              <tr className="bg-alabaster border-b">
+                <td colSpan="3" className=" p-4">
+                  <KPITable />
+                  <KPITable />
+                </td>
+              </tr>
+            </>
+          )}
         </tbody>
       </table>
     </div>
