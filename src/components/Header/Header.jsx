592d436279dc7eb8fc96d171146a235b0e8f0544
aslan997 <aslanisaq997@gmail.com>
2024-07-25 19:51:54 +0500

    remove logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -33,11 +33,11 @@ const Header = () => {
   const years = [2022, 2023, 2024, 2025, 2026, 2027, 2028];
 
   return (
-    <header className="bg-white p-4 shadow-md fixed w-screen z-10">
+    <header className="bg-white p-4 shadow-md fixed min-w-full z-10">
       <div className=" mx-auto flex items-center justify-between">
         <div className="flex space-x-2">
           <Dropdown />
-          <div className="space-x-2 hidden xl:flex">
+          <div className="space-x-2 hidden lg:flex">
             {years?.map((year) => (
               <YearButton
                 key={year}
@@ -48,71 +48,64 @@ const Header = () => {
             ))}
           </div>
         </div>
-        <div className="flex space-x-2 items-center">
-          <div
-            className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-600 cursor-pointer"
-            onClick={handleClick}
+        {/* <div className="flex items-center"> */}
+        <div
+          className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-600 cursor-pointer"
+          onClick={handleClick}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-6 h-6 text-white"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 text-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 4a4 4 0 100 8 4 4 0 000-8zM4 20a8 8 0 0116 0H4z"
-              />
-            </svg>
-          </div>
-          {isOpen && (
-            <div
-              ref={dropdownRef}
-              className="absolute z-1 top-16 right-36 w-44 bg-white border border-gray-200 rounded-md shadow-lg"
-            >
-              <div
-                className="py-1"
-                role="listbox"
-                aria-orientation="vertical"
-                aria-labelledby="menu-button"
-              >
-                <Link
-                  to="/profile"
-                  className="w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700 inline-flex items-center"
-                  role="option"
-                  onClick={() => setIsOpen(false)}
-                >
-                  <Icon
-                    icon="heroicons:user-circle-16-solid"
-                    className="mr-2 text-viewProfileIcon"
-                  />
-                  View Profile
-                </Link>
-                <hr />
-                <Link
-                  to="/login"
-                  className="w-full px-4 py-2 text-sm text-red-500 hover:bg-gray-100 dark:hover:bg-gray-700 inline-flex items-center"
-                  role="option"
-                  onClick={() => setIsOpen(false)}
-                >
-                  <Icon icon="tdesign:poweroff" className="mr-2" />
-                  Logout
-                </Link>
-              </div>
-            </div>
-          )}
-          <div>
-            <img
-              src="https://icd.gov.ae/wp-content/themes/ICD/assets/images/logo.png"
-              alt="ICD Logo"
-              className="h-8"
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 4a4 4 0 100 8 4 4 0 000-8zM4 20a8 8 0 0116 0H4z"
             />
-          </div>
+          </svg>
         </div>
+        {isOpen && (
+          <div
+            ref={dropdownRef}
+            className="absolute z-1 top-16 right-4 w-44 bg-white border border-gray-200 rounded-md shadow-lg"
+          >
+            <div
+              className="py-1"
+              role="listbox"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+            >
+              <Link
+                to="/profile"
+                className="w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700 inline-flex items-center"
+                role="option"
+                onClick={() => setIsOpen(false)}
+              >
+                <Icon
+                  icon="heroicons:user-circle-16-solid"
+                  className="mr-2 text-viewProfileIcon"
+                />
+                View Profile
+              </Link>
+              <hr />
+              <Link
+                to="/login"
+                className="w-full px-4 py-2 text-sm text-red-500 hover:bg-gray-100 dark:hover:bg-gray-700 inline-flex items-center"
+                role="option"
+                onClick={() => setIsOpen(false)}
+              >
+                <Icon icon="tdesign:poweroff" className="mr-2" />
+                Logout
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
+      {/* </div> */}
     </header>
   );
 };
